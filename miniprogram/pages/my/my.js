b6a2486ebb670a04ea2625e8c1d61c2c813c0f17
948511270 <948511270@qq.com>
2023-07-27 21:15:29 +0800

    将“我的”页面中修改信息模块完成

--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -1,33 +1,88 @@
 // pages/my/my.js
+// 级联选择数据 软件/物联 ->23级/24级/25级
+const options = [
+  {
+    text: '软件',
+    value: '01',
+    children: [{ text: '23级', value: '0123' },{ text: '24级', value: '0124' },{ text: '25级', value: '0125' }],
+  },
+  {
+    text: '物联',
+    value: '02',
+    children: [{ text: '23级', value: '0223' },{ text: '24级', value: '0224' },{ text: '25级', value: '0225' }],
+  },
+];
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    show1:false,
-    show2:false
+    // options fieldValue cascaderValue为级联选择相关数据
+    options,
+    fieldValue: '',
+    cascaderValue: '',
+    // 学生id
+    studentId:'',
+    // 控制考核订阅popup弹出层
+    showAssessmentSubscription:false,
+    // 控制修改信息弹出层
+    showModifyInformation:false,
+    // 控制级联选择弹出层
+    showCascader: false,
+    
+    
   },
+
   // 不知道为什么不能传参数 不然我就写到一个方法里了
+  // showPopup1 是考核订阅的popup 
+  // showPopup2 是修改信息的popup
   showPopup1() {
-    this.setData({ show1: true });
+    this.setData({ showAssessmentSubscription: true});
   },
   showPopup2() {
-    this.setData({ show2: true });
+    
+    // 逻辑是 点击修改信息单元格后 将修改信息的弹出层显示，并且将
+    this.setData({ showModifyInformation: true,showCascader:true  });
   },
   closePopup1(){
-    this.setData({ show1: false });
+    this.setData({ showAssessmentSubscription: false });
   },
   closePopup2(){
-    this.setData({ show2: false });
+    this.setData({ showModifyInformation: false });
   },
   urlJump(){
     wx.redirectTo({
       url: '/pages/index/index',
     })
   },
-  onClose() {
-    
+  // 当输入学号的输入框失焦后调用此方法 将学号存入变量studentId
+  onEnterID(event) {
+    // event.detail 为当前输入的值
+    this.data.studentId = event.detail
+  },
+  // 当点击修改信息单元格中的确认修改按钮时调用 将数据输入框与整个修改信息popup关闭
+  onClickModifyInformation(event){
+    this.setData({
+      showField:false,
+      showModifyInformation: false
+    });
+  },
+  // 级联选择完毕后调用 将数据拿到后分割开来 分割格式如下：软件/22级，分割完毕后存入变量中并关闭级联选择弹出层并打开学号输入框
+  onFinish(e) {
+    const { selectedOptions, value } = e.detail;
+    const fieldValue = selectedOptions
+        .map((option) => option.text || option.name)
+        .join('/');
+    this.setData({
+      fieldValue,
+      cascaderValue: value,
+    })
+    this.setData({
+      showCascader:false ,
+      showField:true
+    });
     
   },
   /**
